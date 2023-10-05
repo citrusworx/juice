50a3982d3001b03dd5266739aee37063de0f781d
Drew Marshall <drewwinkles@gmail.com>
2023-10-05 12:57:47 -0500

    0.0.4.1 - Minor Update
    --Structure Change
    --Added CSS and JS files
     (haven't tested in code)

---
+++ js/berillium-native/config.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+
+const reds = StyleSheet.create({
+    bgRed100: {
+        backgroundColor: rgb(242,242,242)
+    },
+    bgRed200: {
+        backgroundColor: rgb(255, 255, 230)
+    },
+    bgRed300: {
+        backgroundColor: rgb(255, 255, 217)
+    },
+    bgRed400: {
+        backgroundColor: rgb(255, 255, 204)
+    },
+    bgRed500: {
+        backgroundColor: rgb(255, 255, 191)
+    },
+    bgRed600: {
+        backgroundColor: rgb(255, 255, 179)
+    },
+    bgRed700: {
+        backgroundColor: rgb(255, 255, 166)
+    },
+    bgRed800: {
+        backgroundColor: rgb(255, 255, 153)
+    },
+    bgRed900: {
+        backgroundColor: rgb(255, 255, 140)
+    }
+});
+
+export default reds;
---
+++ js/berillium-native/flex.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+const flex = StyleSheet.create({
+    flex: {
+        display: flex,
+    }
+});
+
+
+export default flex;
