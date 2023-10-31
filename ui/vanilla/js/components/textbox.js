8af8be0a714d0141556792da6dd386046a9b16de
Drew Marshall <43978216+stinklewinks@users.noreply.github.com>
2023-10-31 17:39:59 -0500

    Updated code comments

--- ui/vanilla/js/components/textbox.js
+++ ui/vanilla/js/components/textbox.js
@@ -1,17 +1,23 @@
 function TextBox(){
+    //Create components
     const aboutDiv = document.createElement('div');
     const heading = document.createElement('h2');
     const subheading = document.createElement('span');
     const content = document.createElement('p');
     const linebreak = document.createElement('br');
     const libr = document.createElement('br');
+
+    //Add content
     heading.textContent = 'Hello';
     subheading.textContent = 'This is your first UI element';
     content.textContent = 'This is the first UI element for berilliumUI: a new package included in berilliumCSS!';
     
+    //append to container
     const page_content = [heading, linebreak, subheading, libr,  content];
     console.log(page_content);
     aboutDiv.append(...page_content);
+
+    //Return container
     return aboutDiv;
 }
 
