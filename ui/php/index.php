3719db5e753db46f5bef9d3eeaf81304aa873b4b
Drew Marshall <drewwinkles@gmail.com>
2024-08-09 10:25:34 -0500

    Added more to swatches. Edited class namings.

---
+++ ui/php/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LemonJuice | phpUI</title>
+</head>
+<body>
+    <?php 
+        function headerOne(){
+            return "
+            
+                <header>
+                    <h2>Hello, world!</h2>
+                    <p>
+                        This is some paragraph text.
+                    </p>
+                </header>
+            
+            ";
+        }
+        echo headerOne();
+    ?>
+
+        
+
+</body>
+</html>
